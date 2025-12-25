f05f640dbaf88fc05fc64ba98a40f3ffbefbd34d
soroardiislam <mdsoroardi602828@gmail.com>
2025-12-25 23:10:53 +0600

    models

--- models/favourite.js
+++ models/favourite.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const favouriteSchema = mongoose.Schema({
   houseId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Home',
+    ref: "Home",
     required: true,
-    unique: true
-  }
+    unique: true,
+  },
 });
 
-module.exports = mongoose.model('Favourite', favouriteSchema);
+module.exports = mongoose.model("Favourite", favouriteSchema);
